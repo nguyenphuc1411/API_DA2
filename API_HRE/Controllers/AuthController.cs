b4d35f65290562dcfd9f0f917d624a5a133fe2ef
Phuc Nguyen <nguyenphuc14112003@gmail.com>
2024-10-31 21:21:25 +0700

    test

---
+++ API_HRE/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_HRE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
